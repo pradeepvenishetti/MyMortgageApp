0a548e45ee08dc78e71016641fecab576088b516
pradeep <pradeep@PRADEEP>
2016-07-31 17:25:14 -0500

    Add project files.

---
+++ MortgageApp.Model/Borrower.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageApp.Model
+{
+    class Borrower
+    {
+
+        public int BorrowerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public bool IsPrimary { get; set; }
+
+    }
+}
